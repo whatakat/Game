b83210ba3dc35426ce1edd9573c87311774b2429
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-25 00:02:05 +0300

    add constructor

--- android/src/com/mygdx/game/ArrowEmitter.java
+++ android/src/com/mygdx/game/ArrowEmitter.java
@@ -1,9 +1,22 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Texture;
+
 public class ArrowEmitter {
     private static final ArrowEmitter ourInstance = new ArrowEmitter();
     public static ArrowEmitter getInstance(){
         return ourInstance;
     }
+    Texture texture;
+    Arrow[] arrows;
+
+    private ArrowEmitter(){
+        texture = new Texture("");
+        arrows = new Arrow[100];
+        for (int i = 0; i <arrows.length ; i++) {
+            arrows[i] = new Arrow();
+        }
+    }
+
 
 }
