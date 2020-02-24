eba932b288e02ab396597194c5ba273ac6a5b079
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-24 23:58:31 +0300

    add ArrowEmitter

---
+++ android/src/com/mygdx/game/ArrowEmitter.java
@@ -0,0 +1,9 @@
+package com.mygdx.game;
+
+public class ArrowEmitter {
+    private static final ArrowEmitter ourInstance = new ArrowEmitter();
+    public static ArrowEmitter getInstance(){
+        return ourInstance;
+    }
+
+}
