8b17eaba8846624d9aa6eaba3d9cc7f0aea5e43a
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-25 11:10:18 +0300

    change Game

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -8,26 +8,40 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Background background;
+	Hero hero;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("background_okean.jpg");
+		background = new Background();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		float dt = Gdx.graphics.getDeltaTime();
+		update(dt);
+		Gdx.gl.glClearColor(1,1,1,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		background.render(batch);
+		//hero.render(batch);
+		//SharkEmitter.getInstance().render(batch);
+		//ArrowEmitter.getInstance().render(batch);
 		batch.end();
-	}
+			}
+			public void update(float dt){
+		background.update(hero,dt);
+		hero.update(dt);
+				//SharkEmitter.getInstance().update(dt);
+				//ArrowEmitter.getInstance().update(dt);
+
+
+			}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		background.dispose();
 	}
 }
