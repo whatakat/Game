f020db5f691e9b104824181c1237175b01555488
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-27 21:12:09 +0300

    refactor

--- core/src/com/mygdx/game/base/BaseScreen.java
+++ core/src/com/mygdx/game/base/BaseScreen.java
@@ -1,13 +1,17 @@
 package com.mygdx.game.base;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 
-public class BaseScreen implements Screen {
+public class BaseScreen implements Screen, InputProcessor {
+
 
     protected Game game;
     public BaseScreen(Game game){
         this.game = game;
+        Gdx.input.setInputProcessor(this);
     }
 
 
@@ -45,5 +49,45 @@ public class BaseScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
