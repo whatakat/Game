cb5ecc5c87a31e1aa92cb381ef23530afda75e6d
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-27 19:54:38 +0300

    refactor

--- core/src/com/mygdx/game/game/SharkGame.java
+++ core/src/com/mygdx/game/game/SharkGame.java
@@ -1,6 +1,11 @@
 package com.mygdx.game.game;
 
 import com.badlogic.gdx.Game;
+import com.mygdx.game.screen.MenuScreen;
 
 public class SharkGame extends Game {
+    @Override
+    public void create() {
+        setScreen(new MenuScreen());
+    }
 }
--- core/src/com/mygdx/game/screen/MenuScreen.java
+++ core/src/com/mygdx/game/screen/MenuScreen.java
@@ -3,4 +3,5 @@ package com.mygdx.game.screen;
 import com.mygdx.game.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
+
 }
