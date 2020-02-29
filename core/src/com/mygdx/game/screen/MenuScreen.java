6248abeaaf089086a4b091e887079d504d70da72
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-29 11:07:26 +0300

    test

--- core/src/com/mygdx/game/screen/MenuScreen.java
+++ core/src/com/mygdx/game/screen/MenuScreen.java
@@ -5,11 +5,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.base.BaseScreen;
+import com.mygdx.game.base.Sprite;
 
 public class MenuScreen extends BaseScreen {
     Texture img;
+    Sprite logo;
 
     public MenuScreen(Game game){
         super(game);
@@ -20,6 +23,9 @@ public class MenuScreen extends BaseScreen {
     public void show() {
         super.show();
         img = new Texture("background_okean.jpg");
+        logo = new Sprite(new TextureRegion(img));
+        logo.setSize(2f,1f);
+
     }
 
     @Override
@@ -28,7 +34,7 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img,-1f,-1f,2f,2f);
+        logo.draw(batch);
         batch.end();
     }
 
