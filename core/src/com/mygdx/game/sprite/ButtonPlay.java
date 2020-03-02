ce0c357124a97b6986a069c9cb2527ac93b63bb5
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-02 22:45:36 +0300

    buttonPlay position

--- core/src/com/mygdx/game/sprite/ButtonPlay.java
+++ core/src/com/mygdx/game/sprite/ButtonPlay.java
@@ -3,12 +3,17 @@ package com.mygdx.game.sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.mygdx.game.base.ActionListener;
 import com.mygdx.game.base.ScaledTouchUpButton;
+import com.mygdx.game.math.Rect;
 
 public class ButtonPlay extends ScaledTouchUpButton {
 
     public ButtonPlay(TextureAtlas atlas, ActionListener actionListener, float pressScale) {
         super(atlas.findRegion("play"),actionListener,pressScale);
-
     }
 
+    @Override
+    public void resize(Rect worldBounds) {
+        setBottom(worldBounds.getBottom());
+        setRight(worldBounds.getRight());
+    }
 }
