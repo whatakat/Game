c5f2d0c9af4a75a79a994d2e5ba30e2b8eb7a879
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-02 22:35:22 +0300

    position bottom in resize() ButtonExit

--- core/src/com/mygdx/game/sprite/ButtonExit.java
+++ core/src/com/mygdx/game/sprite/ButtonExit.java
@@ -3,13 +3,17 @@ package com.mygdx.game.sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.mygdx.game.base.ActionListener;
 import com.mygdx.game.base.ScaledTouchUpButton;
+import com.mygdx.game.math.Rect;
 
 public class ButtonExit extends ScaledTouchUpButton {
 
     public ButtonExit(TextureAtlas atlas, ActionListener actionListener, float pressScale) {
         super(atlas.findRegion("exit"),actionListener,pressScale);
-
-
     }
 
+    @Override
+    public void resize(Rect worldBounds) {
+        setBottom(worldBounds.getBottom());
+        setLeft(worldBounds.getLeft());
+    }
 }
