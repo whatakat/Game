2e4946fac5f8e437055e6c1223ba3da2da6b00e5
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-06 17:16:09 +0300

    add constructor

--- core/src/com/mygdx/game/base/Ship.java
+++ core/src/com/mygdx/game/base/Ship.java
@@ -24,6 +24,11 @@ public class Ship extends Sprite {
     public Ship(TextureRegion region, int rows, int cols, int frames) {
         super(region, rows, cols, frames);
     }
+    public Ship(ArrowPool arrowPool, Rect worldBounds) {
+        this.arrowPool = arrowPool;
+        this.worldBounds = worldBounds;
+
+    }
 
     @Override
     public void resize(Rect worldBounds) {
