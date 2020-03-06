afec084a22d658f6e719c58d29f93fd4493bead3
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-06 17:19:25 +0300

    Enemy update

--- core/src/com/mygdx/game/sprite/Enemy.java
+++ core/src/com/mygdx/game/sprite/Enemy.java
@@ -1,12 +1,21 @@
 package com.mygdx.game.sprite;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.base.Ship;
 import com.mygdx.game.math.Rect;
 import com.mygdx.game.pools.ArrowPool;
 
 public class Enemy extends Ship {
-    public Enemy(ArrowPool arrowPool, Rect worldBounds) {
 
+    private Vector2 v0 = new Vector2();
+    public Enemy(ArrowPool arrowPool, Rect worldBounds) {
+        super(arrowPool, worldBounds);
+        this.v.set(v0);
+    }
+
+    @Override
+    public void update(float delta) {
+        super.update(delta);
     }
 }
