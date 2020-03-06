f6c74ad05c9026dbca3402b0dcdcb23d99fb504a
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-06 17:13:12 +0300

    add Enemy

---
+++ core/src/com/mygdx/game/sprite/Enemy.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.sprite;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.mygdx.game.base.Ship;
+import com.mygdx.game.math.Rect;
+import com.mygdx.game.pools.ArrowPool;
+
+public class Enemy extends Ship {
+    public Enemy(ArrowPool arrowPool, Rect worldBounds) {
+
+    }
+}
