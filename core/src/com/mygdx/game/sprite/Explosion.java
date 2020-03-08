0cdf68ab0814b3268796e82cf3571af36abe1635
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-08 09:42:02 +0300

    add Explosion

---
+++ core/src/com/mygdx/game/sprite/Explosion.java
@@ -0,0 +1,35 @@
+package com.mygdx.game.sprite;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.base.Sprite;
+
+public class Explosion extends Sprite {
+    private float animateInterval = 0.017f;
+    private float animateTimer;
+
+    public Explosion(TextureRegion region,int rows,int cols, int frames){
+        super(region, rows, cols, frames);
+    }
+    public void set(float height, Vector2 pos){
+        this.pos.set(pos);
+        setHeightProportion(height);
+    }
+
+    @Override
+    public void update(float delta) {
+        animateTimer+=delta;
+        if (animateTimer>=animateInterval){
+            animateTimer = 0f;
+            if (++frame==regions.length){
+                destroy();
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        frame = 0;
+    }
+}
