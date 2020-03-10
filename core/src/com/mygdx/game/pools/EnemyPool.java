4f51c94b2a2b1a9645048c5e678f68ebb5ca45db
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-10 23:26:28 +0300

    correct EnemyPool

--- core/src/com/mygdx/game/pools/EnemyPool.java
+++ core/src/com/mygdx/game/pools/EnemyPool.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.pools;
 
+import com.badlogic.gdx.audio.Sound;
 import com.mygdx.game.base.SpritesPool;
 import com.mygdx.game.math.Rect;
 import com.mygdx.game.sprite.Enemy;
@@ -8,13 +9,15 @@ public class EnemyPool extends SpritesPool<Enemy> {
     private ArrowPool arrowPool;
     private ExplosionPool explosionPool;
     private Rect worldBounds;
-    public EnemyPool (ArrowPool arrowPool, Rect worldBounds, ExplosionPool explosionPool){
+    private Sound sound;
+    public EnemyPool (ArrowPool arrowPool, Rect worldBounds, ExplosionPool explosionPool, Sound sound){
         this.arrowPool = arrowPool;
         this.worldBounds = worldBounds;
         this.explosionPool = explosionPool;
+        this.sound = sound;
     }
     @Override
     protected Enemy newObject() {
-        return new Enemy(arrowPool,worldBounds,explosionPool);
+        return new Enemy(arrowPool,worldBounds,explosionPool, sound);
     }
 }
