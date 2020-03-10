f9ee8366f9075512669243757b054ad89cfbc48b
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-10 22:24:24 +0300

    add sound ExplosionPool

--- core/src/com/mygdx/game/pools/ExplosionPool.java
+++ core/src/com/mygdx/game/pools/ExplosionPool.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.pools;
 
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.mygdx.game.base.SpritesPool;
@@ -7,14 +8,17 @@ import com.mygdx.game.sprite.Explosion;
 
 public class ExplosionPool extends SpritesPool<Explosion> {
 
+
+    private Sound sound;
     private TextureRegion textureRegion;
 
-    public ExplosionPool(TextureAtlas atlas) {
-        textureRegion = atlas.findRegion("shark");
+    public ExplosionPool(TextureAtlas atlas, Sound sound) {
+        this.textureRegion = atlas.findRegion("shark");
+        this.sound = sound;
     }
 
     @Override
     protected Explosion newObject() {
-        return new Explosion(textureRegion,5,5,25);
+        return new Explosion(textureRegion,5,5,25, sound);
     }
 }
